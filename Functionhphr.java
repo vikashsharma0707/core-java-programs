65356550a751c023516040ef4a4bd6db192a3823
vikash sharma <vikashtiwari9893@gmail.com>
2024-07-03 00:33:44 +0530

    twenty commit

---
+++ Functionhphr.java
@@ -0,0 +1,13 @@
+  class Functionhphr{
+        public static int Add(int a,int b){
+        int c;
+        c=a+b;
+       return c;
+    }
+
+   public static void main(String args[]){
+          int result= Add(2,3);
+    System.out.println("Addition of numbers is :" +result);
+  }
+ }
+ 
---
+++ Functionhrnp.java
@@ -0,0 +1,16 @@
+  class Functionhrnp{
+        public static int Add(){
+         int c;
+         int a=2; 
+         int b=3;
+      c=a+b;
+
+    return c;
+  }
+
+    public static void main(String args[]){
+           int result =Add();
+     System.out.println("Addition of numbers is :" +result);
+  }
+ }
+      
