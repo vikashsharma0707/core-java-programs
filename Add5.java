00e48373e79307f4a79f39ceea45c75e1657cb14
vikash sharma <vikashtiwari9893@gmail.com>
2024-07-09 23:58:30 +0530

    Add commit

---
+++ Add5.java
@@ -0,0 +1,17 @@
+
+
+  class Add5{
+        public static void main(String args[]){
+        int a,b,c;
+         a=2;
+         b=3;
+
+         c=a+b;
+
+        System.out.println("Addition of two numbers is:" +c);
+   
+      }
+    }
+  
+         
+     
---
+++ RGPV.java
@@ -0,0 +1,47 @@
+
+  abstract class RGPV{
+           public static void lab();
+           public static void cctv();
+       // abstract
+
+     public void examCopyCheck(){
+             System.out.println("This is RGPV non abstract method ");
+    }
+
+    public RGPV(){
+           System.out.println("this is RGPV constructor ");
+     }
+  }
+
+   public College extends RGPV{
+          public void cctv(){
+              System.out.println("This is cctv method rule by RGPV");
+          }
+
+         public void lab(){
+              System.out.println("This is lab method rule by RGPV");
+          }
+
+         public void staff(){
+              System.out.println("This is college method rule by RGPV");
+          }
+
+         public College(){
+              System.out.println("This is college constructor");
+          }
+
+          public static void main(String args[]){
+           RGPV obj =new College();
+           obj.cctv();
+           obj.lab();
+           obj.examCopyCheck();
+           College c=new College();
+           c.cctv();
+           c.lab();
+           c.examCopyCheck();
+           c.staff();
+       }
+     }
+         
+
+          
