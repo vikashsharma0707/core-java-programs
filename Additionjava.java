60ac233124fea2d87a3a776e2aa84229f29997c0
vikash sharma <vikashtiwari9893@gmail.com>
2024-07-14 18:56:11 +0530

    Addition commit

---
+++ Additionjava.java
@@ -0,0 +1,14 @@
+
+
+  class Additionjava{
+        public static void main(String args[]){
+        int a,b,c;
+        a=2;
+        b=3;
+        c=2+3;
+
+     System.out.println("Addition of two number is :" +c);
+
+   }
+ }
+   
