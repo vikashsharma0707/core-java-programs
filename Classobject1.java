b898b8d67fc44627a369657f58c4de6c4d88aa3d
vikash sharma <vikashtiwari9893@gmail.com>
2024-07-16 00:02:32 +0530

    class and object second example

---
+++ Classobject1.java
@@ -0,0 +1,18 @@
+
+  class Classobject1{
+        
+        public void show(){
+         System.out.println("this is show function ");
+       }
+     
+        public void show1(){
+         System.out.println("this is show1 function ");
+      }
+
+   public static void main(String args[]){
+      Classobject1 obj= new Classobject1();
+      obj.show();
+      obj.show1();
+   }
+
+  }
