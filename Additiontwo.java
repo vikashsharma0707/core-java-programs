4dad0cd8ec51a81bc8e8acc9462df6e2e5fb36b2
vikash sharma <vikashtiwari9893@gmail.com>
2024-08-12 23:20:19 +0530

    Addition of two numbers

---
+++ Additiontwo.java
@@ -0,0 +1,13 @@
+
+
+ class Additiontwo{
+       public static void main(String args[]){
+       int a=2;
+       int b=3;
+       int sum=a+b;
+
+  System.out.println("sum of two numbers is :" +sum);
+
+ }
+ 
+}
